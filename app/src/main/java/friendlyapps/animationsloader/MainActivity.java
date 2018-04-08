3fb2c1747dd2124ff41731cf0ed71d2f3bea70e6
Herostrates94 <p.birgiel@gmail.com>
2018-04-08 16:40:33 +0200

    The pictures are now downloaded from Dropbox (not copied from this application's resources

--- app/src/main/java/friendlyapps/animationsloader/MainActivity.java
+++ app/src/main/java/friendlyapps/animationsloader/MainActivity.java
@@ -1,29 +1,33 @@
 package friendlyapps.animationsloader;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.users.FullAccount;
+
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.channels.FileChannel;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final String storageAppMainDirectoryName = "happyApplicationsAnimations";
-    private final String picturesDirectoryName = "pictures";
-    private final String backgroundDirectoryName = "background";
-    private final String animationMovementsDirectoryName = "animationMovements";
-
-    private File storageMainDirectory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,93 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void loadAnimations(View v){
 
-        prepareAppDirectoryInExternalStorage();
+        new AnimationsDownloader().execute();
 
-        try {
-            copyDirectoryFromAssetsToExternalStorage(picturesDirectoryName);
-            copyDirectoryFromAssetsToExternalStorage(animationMovementsDirectoryName);
-            copyDirectoryFromAssetsToExternalStorage(backgroundDirectoryName);
-            //colors?
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        TextView messageTextView = findViewById(R.id.textView);
-        messageTextView.setText("Animations loaded. You can uninstall this app now.");
-
-
-    }
-
-    private void copyDirectoryFromAssetsToExternalStorage(String directoryName) throws IOException {
-
-        createDirectoryInExternalStorageIfNecessary(directoryName);
-
-        String[] assetsIWant;
-        assetsIWant = getAssets().list(directoryName);
-
-        for(String fileName : assetsIWant) {
-
-            //if fileName cointains a dot it means it is a file, not directory
-
-            if(fileName.contains(".")){
-                copyFile(directoryName + java.io.File.separator + fileName);
-            }
-            else{
-                copyDirectoryFromAssetsToExternalStorage(directoryName +
-                        java.io.File.separator + fileName);
-            }
-        }
-
-        //getAssets().open("pictures/butterfly_red.png");
-    }
-
-    private void createDirectoryInExternalStorageIfNecessary(String directoryName){
-
-        File newDirectory = new File(storageMainDirectory, directoryName);
-
-        if (!newDirectory.exists()) {
-            newDirectory.mkdir();
-            Log.i("Files", directoryName + " directory was created");
-        }
-    }
-
-
-    private void prepareAppDirectoryInExternalStorage(){
-
-        storageMainDirectory = new File(Environment.getExternalStorageDirectory(), storageAppMainDirectoryName);
-
-        if (!storageMainDirectory.exists()) {
-            storageMainDirectory.mkdir();
-            Log.i("Files", storageAppMainDirectoryName + " directory was created");
-        }
-    }
-
-    private void copyFile(String destinationPath) throws IOException {
-
-        AssetManager assetManager = getAssets();
-        AssetFileDescriptor assetFileDescriptor;
-        try {
-            assetFileDescriptor = assetManager.openFd(destinationPath);
-
-            InputStream in = assetFileDescriptor.createInputStream();
-            FileOutputStream out = new FileOutputStream(Environment.getExternalStorageDirectory() +
-                    java.io.File.separator + storageAppMainDirectoryName + File.separator + destinationPath);
-            byte[] buff = new byte[1024];
-            int read = 0;
-
-            try {
-                while ((read = in.read(buff)) > 0) {
-                    out.write(buff, 0, read);
-                }
-            } finally {
-                in.close();
-                out.close();
-            }
-
-        } catch (IOException e) {
-            Log.e("Files", destinationPath + " failed " + e.getMessage());
-        }
     }
 
 }
